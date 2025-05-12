430294350f034c76910bdc99b7aaca7bc4db5f45
Maxim Rese <maxim.rese@rdf.nuernberg.de>
2025-05-12 15:08:05 +0200

    hab sqlbfehle durch database_sql ersetzt

---
+++ database_SQL.py
@@ -0,0 +1,46 @@
+import mariadb
+from mariadb import Error
+
+def create_connection():
+    try:
+        connection = mariadb.connect(
+            host="localhost",  # oder Server-IP
+            user="dein_user",
+            password="dein_passwort",
+            database="gamestat_db",
+            port=3306  # Standard-Port für MariaDB
+        )
+        return connection
+    except Error as e:
+        print(f"Fehler bei der Verbindung zur MariaDB: {e}")
+        return None
+    
+def registrieren_ausfuehren(nutzer):
+    connection = create_connection()
+    if not connection:
+        return False
+        
+    try:
+        cursor = connection.cursor()
+        cursor.execute("""
+            INSERT INTO users 
+            (username, password_hash, email, geburtsdatum, geschlecht, land, sprache)
+            VALUES (?, ?, ?, ?, ?, ?, ?)
+        """, (
+            nutzer.nutzername,
+            #generate_password_hash(nutzer.passwort),
+            nutzer.email,
+            nutzer.geburtsdatum,
+            nutzer.geschlecht,
+            nutzer.land,
+            nutzer.sprache
+        ))
+        connection.commit()
+        return True
+        
+    except mariadb.Error as e:
+        print(f"Registrierungsfehler: {e}")
+        return False
+        
+    finally:
+        connection.close()
