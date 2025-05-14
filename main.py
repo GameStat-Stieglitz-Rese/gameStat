74b5db7471fcc94ee12de6ee6678931915e725cf
Marc Stieglitz <marc.stieglitz@rdf.nuernberg.de>
2025-05-14 12:55:25 +0200

    Anpassung main

--- main.py
+++ main.py
@@ -2,41 +2,52 @@ import tkinter as tk
 from tkinter import ttk
 import elemente # Hier sind die Elemente im Fenster (Klöpfe und co. gespeichert)
 from functools import partial
-import anmeldung # Testweise
+#import anmeldung # Testweise
 
+class Testobjekt_main:
+    def __init__(self):
+        self.benutzer = "maxim"
+        self.passwort = "1234"
+        self.vorname = "Maxim"
+        self.email = "maxim@gmail.com"
+        self.land = "Deutschland"
+        self.sprache = "deutsch"
+        self.geschlecht = "Männlich"
+        self.geburtsdatum = "1234"
 
-class HauptBedienung(tk.Frame):
-    def __init__(self, root, user):
-        self.uebersicht = ttk.Button(root, text="Übersicht", command=partial(self.uebersicht, user))
-        self.bewertungen = ttk.Button(root, text="Bewertungen", command=1)
-        self.durchgespielt = ttk.Button(root, text="Durchgespielt", command=1)
-        self.empfohlen = ttk.Button(root, text="Empfohlene Spiele", command=1)
-        self.spiel_hinzufg = ttk.Button(root, text="Spiel Hinzufügen", command=1)
+class HauptBedienung:
+    def __init__(self, root, nutzer):
+        self.bn_uebersicht = ttk.Button(root, text="Übersicht", command=partial(self.uebersicht, nutzer))
+        self.bn_bewertungen = ttk.Button(root, text="Bewertungen", command=1)
+        self.bn_durchgespielt = ttk.Button(root, text="Durchgespielt", command=1)
+        self.bn_empfohlen = ttk.Button(root, text="Empfohlene Spiele", command=1)
+        self.bn_spiel_hinzufg = ttk.Button(root, text="Spiel Hinzufügen", command=1)
 
     def generieren(self, xstart, ystart):
         xwert = xstart
         ywert = ystart
         xadd = 0
         yadd = 25
-        self.uebersicht.place(x=xwert, y=ywert)
+        self.bn_uebersicht.place(x=xwert, y=ywert)
         ywert += yadd
-        self.bewertungen.place(x=xwert , y=ywert )
+        self.bn_bewertungen.place(x=xwert , y=ywert )
         ywert += yadd
-        self.durchgespielt.place(x=xwert, y=ywert)
+        self.bn_durchgespielt.place(x=xwert, y=ywert)
         ywert += yadd
-        self.empfohlen.place(x=xwert, y=ywert)
+        self.bn_empfohlen.place(x=xwert, y=ywert)
         ywert += yadd
-        self.spiel_hinzufg.place(x=xwert, y=ywert)
+        self.bn_spiel_hinzufg.place(x=xwert, y=ywert)
+
+    def uebersicht(nutzer):
+        print("Übersicht")
 
 
 root = tk.Tk()
 root.geometry("900x700")
 root.title("GameStat - Dein Spielemanager")
 
-user = anmeldung.Nutzer()
-
-menue_buttons = HauptBedienung(root, user)
-
+nutzer = Testobjekt_main()
+menue_buttons = HauptBedienung(root, nutzer)
 menue_buttons.generieren(50, 50)
 
 root.mainloop()
